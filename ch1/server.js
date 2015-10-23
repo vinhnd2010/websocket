5736ab63d39095803d6108df1c77135c60b26c97
vinhnd2010 <nguyenducinh92@gmail.com>
2015-10-23 23:59:49 +0700

    Edit
    please enter the commit message for your changes. Lines starting

--- ch1/server.js
+++
@@ -1,9 +0,0 @@
-var WebSocketServer = require("ws").Server,
-  wss = new WebSocketServer({port: 8181});
-
-wss.on("connection", function(ws) {
-  console.log('--------- Client connected');
-  ws.on('message', function(message) {
-    console.log(message);
-  })
-})
